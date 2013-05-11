c58559f36b653cdcbe046edabfa74b01ccdc2a2c
MrTux <mrtux@ubuntu-vn.org>
2013-05-12 03:27:13 +0700

    Update script for Debian Wheezy

--- update-debian-hdmedia.sh
+++ update-debian-hdmedia.sh
@@ -2,15 +2,15 @@
 # Filename: update-debian-hdmedia.sh
 # Description: Update hdmedia images file for install from HDD
 
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-amd64/current/images/hd-media/vmlinuz -O amd64.vmlinuz
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-amd64/current/images/hd-media/initrd.gz -O amd64.initrd.gz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-amd64/current/images/hd-media/vmlinuz -O amd64.vmlinuz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-amd64/current/images/hd-media/initrd.gz -O amd64.initrd.gz
 
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-amd64/current/images/hd-media/gtk/vmlinuz -O amd64.gtk.vmlinuz
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-amd64/current/images/hd-media/gtk/initrd.gz -O amd64.gtk.initrd.gz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-amd64/current/images/hd-media/gtk/vmlinuz -O amd64.gtk.vmlinuz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-amd64/current/images/hd-media/gtk/initrd.gz -O amd64.gtk.initrd.gz
 
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-i386/current/images/hd-media/vmlinuz -O i386.vmlinuz
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-i386/current/images/hd-media/initrd.gz -O i386.initrd.gz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-i386/current/images/hd-media/vmlinuz -O i386.vmlinuz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-i386/current/images/hd-media/initrd.gz -O i386.initrd.gz
 
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-i386/current/images/hd-media/gtk/vmlinuz -O i386.gtk.vmlinuz
-wget http://ftp.debian.org/debian/dists/squeeze/main/installer-i386/current/images/hd-media/gtk/initrd.gz -O i386.gtk.initrd.gz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-i386/current/images/hd-media/gtk/vmlinuz -O i386.gtk.vmlinuz
+wget http://ftp.debian.org/debian/dists/wheezy/main/installer-i386/current/images/hd-media/gtk/initrd.gz -O i386.gtk.initrd.gz
 
